9dcb45ea826aa750ad154c0e47bb871065a0fb09
kareemm00 <karimmohamed4624@gmail.com>
2023-08-10 18:21:02 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - start of program
+ *
+ * Description: a c program that print a string with printf function
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
