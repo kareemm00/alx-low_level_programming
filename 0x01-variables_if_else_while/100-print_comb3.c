84e373ac8c4f186873a928fc985edb4a1d5f242d
kareemm00 <karimmohamed4624@gmail.com>
2023-08-11 19:31:25 -0700

    combination_two_digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -22,13 +22,15 @@ int main(void)
 			{
 				putchar(num1 + '0');
 				putchar(num2 + '0');
-				if (num1 != '8' && num2 != '9')
+				if (num1 == '8' && num2 == '9')
+				{
+					putchar('\n');
+				}
+				else
 				{
 					putchar(',');
 					putchar(' ');
 				}
-				else
-					putchar('\n');
 			}
 			num2++;
 		}
