ddc847027598aaf78b16d04893433f4c3fb1e69a
kareemm00 <karimmohamed4624@gmail.com>
2023-08-11 18:19:39 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - start of the program
+ *
+ * Description: a c program that prints all alphabets in reverse
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char alpha = 'z';
+
+	while (alpha <= 'a')
+	{
+		putchar(alpha);
+		alpha--;
+	}
+	putchar('\n');
+	return (0);
+}
