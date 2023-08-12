db72c0c08a46b7a1cca9d1fd5a7b535318a85ed6
kareemm00 <karimmohamed4624@gmail.com>
2023-08-11 19:25:27 -0700

    combination_two_digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,10 +13,10 @@ int main(void)
 {
 	int num1 = 0, num2;
 
-	while (num1 < 10)
+	while (num1 <= 9)
 	{
 		num2 = 0;
-		while (num2 < 10)
+		while (num2 <= 9)
 		{
 			if (num1 != num2 && num1 < num2)
 			{
