1112efc27b74329086b4cb572a34e5636922cff2
kareemm00 <karimmohamed4624@gmail.com>
2023-08-15 19:31:46 -0700

    timetable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,6 +11,7 @@ void times_table(void)
 
 	for (x = 0; x <= 9; x++)
 	{
+		_putchar(48);
 		for (y = 0; y <= 9; y++)
 		{
 			_putchar(',');
@@ -20,8 +21,8 @@ void times_table(void)
 			if (z <= 9)
 				_putchar(' ');
 			else
-				_putchar((z / 10) + '0');
-			_putchar((z % 10) + '0');
+				_putchar((z / 10) + 48);
+			_putchar((z % 10) + 48);
 		}
 		_putchar('\n');
 	}
