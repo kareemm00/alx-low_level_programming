7f3cce30c7380d6b10b8fa85e7b204f068523dd8
kareemm00 <karimmohamed4624@gmail.com>
2023-08-17 19:11:51 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,9 +12,9 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if (num % 3 == 0)
+		if (num % 3 == 0 && !(num % 5 == 0))
 			printf("Fizz");
-		else if (num % 5 == 0)
+		else if (num % 5 == 0 && !(num % 3 == 0))
 			printf("Buzz");
 		else if (num % 3 == 0 && num % 5 == 0)
 			printf("FizzBuzz");
@@ -23,7 +23,8 @@ int main(void)
 
 		if (num != 100)
 			printf(" ");
+		else
+			printf("\n");
 	}
-	printf("\n");
 	return (0);
 }
