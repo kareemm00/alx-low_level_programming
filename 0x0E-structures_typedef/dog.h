2081ba95603fe1e0ccc6578a2c29142c989df8e2
kareemm00 <karimmohamed4624@gmail.com>
2023-09-11 19:37:28 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct dog
+ *
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: a struct dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
