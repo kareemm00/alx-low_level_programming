ab96c872b5bbba661227097001993a96a6ea8a58
kareemm00 <karimmohamed4624@gmail.com>
2023-09-26 12:42:46 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,14 +10,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t k = 0;
+	size_t i = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		k++;
+		i++;
 	}
 
-	return (k);
+	return (i);
 }
